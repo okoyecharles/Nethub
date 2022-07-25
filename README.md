835de2a2d95b06477c399f33b0588043edeb157f
Okoye Charles <okoyecharles509@gmail.com>
2022-07-25 09:59:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Webpack-BoilerPlate
+This is a simple yet powerful Webpack boilerplate, which i will use as a starting point in my projects. 
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/template.html' })],
+};

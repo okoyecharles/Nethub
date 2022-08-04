e56c77b5acd2d885cf0f37f89b9b9a0c8dda1082
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-04 15:44:47 +0100

    Add styles to crumb bar

--- src/modules/loadComments.js
+++ src/modules/loadComments.js
@@ -4,7 +4,7 @@ import getTimeSpan from "./convertDate.js";
 const loadComments = async (id) => {
   const data = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments?item_id=${id}`);
   const comments = await data.json();
-
+  
   return comments;
 };
 
@@ -16,27 +16,33 @@ const addComments = async () => {
       const commentsUI = document.querySelector('.comments')
       commentsUI.innerHTML = '';
       loadComments(popup.dataset.commentPopupIndex)
-      .then(data => {
-        const comments = data;
-        comments.reverse();
+        .then(data => {
+          const comments = data;
+          if (comments.length) {
+            comments.reverse();
 
-        comments?.forEach(comment => {
-          const listItem = document.createElement('li');
-          listItem.className = 'comment';
+            comments?.forEach(comment => {
+              const listItem = document.createElement('li');
+              listItem.className = 'comment';
 
-          const date = document.createElement('span');
-          date.innerHTML = getTimeSpan(comment.creation_date, new Date());
-          const user = document.createElement('div');
-          user.innerText = comment.username;
-          const message = document.createElement('div');
-          message.innerHTML = comment.comment;
+              const date = document.createElement('time');
+              date.innerText = getTimeSpan(comment.creation_date, new Date());
+              date.dateTime = comment.creation_date;
+              const user = document.createElement('div');
+              user.innerText = comment.username;
+              const message = document.createElement('div');
+              message.innerText = comment.comment;
 
-          listItem.append(date, user, message)
-          commentsUI.append(listItem)
+              listItem.append(date, user, message)
+              commentsUI.append(listItem)
+            })
+          }
         })
-      })
     })
   })
 }
 
-export { addComments, loadComments }
+export {
+  addComments,
+  loadComments
+}
